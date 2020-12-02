b53899434418d2212d1ea69cadf1a72d3359ac82
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2020-12-01 21:11:36 -0300

    RaizQuadrada

---
+++ DesingPatterns/DesignPatterns2/DesignPatterns2/Cap4/RaizQuadrada.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns2.Cap4
+{
+    public class RaizQuadrada : IExpressao
+    {
+        private IExpressao expressao;
+
+        public RaizQuadrada(IExpressao e)
+        {
+            this.expressao = e;
+        }
+
+        public int Avalia()
+        {
+            return (int)Math.Sqrt(expressao.Avalia());
+        }
+    }
+}
