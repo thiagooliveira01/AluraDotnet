a411dc2304440a779a4ee3be7d2b4f2fbcb66556
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-01-11 20:47:57 -0300

    Aula 1 C#: Testes de unidade e TDD com xUnit Promovendo rápido feedback

---
+++ TesteComXUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
+        }
+        private static void Verifica(double esperado, double obtido)
+        {
+            var cor = Console.ForegroundColor;
+            if (esperado == obtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(
+                    $"TESTE FALHOU! Esperado: {esperado}, obtido: {obtido}.");
+            }
+            Console.ForegroundColor = cor;
+        }
+        private static void LeilaoComApenasUmLance()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            leilao.RecebeLance(fulano, 800);
+
+            //Act = método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            Verifica(800, leilao.Ganhador.Valor);
+        }
+        private static void LeilaoComVariosLances()
+        {
+            //Arranje = cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(fulano, 990);
+
+            //Act = método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            Verifica(1000, leilao.Ganhador.Valor);
+        }
+    }
+}
