be9a626196ac8d7e12e9fe21ee7aeb1df1fad11c
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-01-13 19:43:02 -0300

    Aula 2 Organizando seus testes Aula 3 Não deixei o sistema regredir

--- TesteComXUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.Core/Leilao.cs
+++ TesteComXUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.Core/Leilao.cs
@@ -3,32 +3,55 @@ using System.Linq;
 
 namespace Alura.LeilaoOnline.Core
 {
+    public enum EstadoLeilao
+    {
+        LeilaoAntesPregao,
+        LeilaoEmAndamento,
+        LeilaoFinalizado
+    }
     public class Leilao
     {
+        private Interessada _ultimoCliente;
         private IList<Lance> _lances;
         public IEnumerable<Lance> Lances => _lances;
         public string Peca { get; }
         public Lance Ganhador { get; private set; }
+        public EstadoLeilao Estado { get; private set; }
 
         public Leilao(string peca)
         {
             Peca = peca;
             _lances = new List<Lance>();
+            Estado = EstadoLeilao.LeilaoAntesPregao;
+        }
+       
+        private bool NovoLanceEhAceito(Interessada cliente, double valor)
+        {
+            return (Estado == EstadoLeilao.LeilaoEmAndamento) && (cliente != _ultimoCliente);
         }
 
         public void RecebeLance(Interessada cliente, double valor)
         {
-            _lances.Add(new Lance(cliente, valor));
+            if (NovoLanceEhAceito(cliente, valor)) 
+            {
+                _lances.Add(new Lance(cliente, valor));
+                _ultimoCliente = cliente;
+            }              
         }
 
         public void IniciaPregao()
         {
-
+            Estado = EstadoLeilao.LeilaoEmAndamento;
         }
 
         public void TerminaPregao()
         {
-            Ganhador = Lances.OrderBy(l => l.Valor).Last();
+            Ganhador = Lances
+                .DefaultIfEmpty(new Lance(null, 0))
+                .OrderBy(l => l.Valor)
+                .LastOrDefault();
+
+            Estado = EstadoLeilao.LeilaoFinalizado;
         }
     }
 }
---
+++ TesteComXUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoRecebeLance.cs
@@ -0,0 +1,75 @@
+﻿using Alura.LeilaoOnline.Core;
+using System.Linq;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoRecebeLance
+    {
+        [Fact]
+        public void NaoAceitaProximoLanceDadoMesmoClienteRealizouUltimo()
+        {
+            //Arranje = cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            leilao.IniciaPregao();
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act = método sob teste
+            leilao.RecebeLance(fulano, 1000);
+
+            //Assert
+            var qtdeEperada = 1;
+            Assert.Equal(qtdeEperada, leilao.Lances.Count());
+        }
+        [Theory]
+        [InlineData(2, new double[] { 800, 900})]
+        [InlineData(3, new double[] { 800, 900, 1000})]
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado(int qtdeEperada, double[] ofertas)
+        {
+            //Arranje = cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            leilao.IniciaPregao();
+            for (int i = 0; i < ofertas.Length; i++)
+            {
+                var valor = ofertas[i];
+                if ((i % 2) == 0)
+                {
+                    leilao.RecebeLance(fulano, valor);
+                }
+                else
+                {
+                    leilao.RecebeLance(maria, valor);
+                }
+            }
+            leilao.TerminaPregao();
+
+            //Act = método sob teste
+            leilao.RecebeLance(fulano, 1000);
+
+            //Assert
+            Assert.Equal(qtdeEperada, leilao.Lances.Count());
+        }
+
+        [Theory]
+        [InlineData(new double[] { 200, 300, 400, 500 })]
+        [InlineData(new double[] { 200 })]
+        [InlineData(new double[] { 200, 300, 400 })]
+        [InlineData(new double[] { 200, 300, 400, 500, 600, 700 })]
+        public void QtdePermaneceZeroDadoQuePregaoNaoFoiIniciado(double[] ofertas)
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano de Tal", leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+
+            Assert.Empty(leilao.Lances);
+        }
+    }
+}
