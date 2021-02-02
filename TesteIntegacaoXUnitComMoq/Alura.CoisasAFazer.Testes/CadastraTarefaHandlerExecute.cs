4268454991b93a55ec7f8b1216405d3b0b8c1b86
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-02-02 20:35:36 -0300

    Capitulo3

--- TesteIntegacaoXUnitComMoq/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
+++ TesteIntegacaoXUnitComMoq/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using Xunit;
+using Moq;
 
 namespace Alura.CoisasAFazer.Testes
 {
@@ -32,6 +33,27 @@ namespace Alura.CoisasAFazer.Testes
             var tarefas = repo.ObtemTarefas(t => t.Titulo == "Estudar Xunit").FirstOrDefault();
             Assert.NotNull(tarefas);
 
+        }
+
+        [Fact]
+        public void QuandoExceptionForLancadaResultadoDeveSerFalse()
+        {
+            //arrange
+            var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
+
+            var mock = new Mock<IRepositorioTarefas>();
+
+            mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>())).Throws(new Exception("Houve um erro ao incluir Tarefas"));
+
+            var repo = mock.Object;
+
+            var handle = new CadastraTarefaHandler(repo);
+
+            //act
+            CommandResult resultado = handle.Execute(comando);
+
+            //assert
+            Assert.False(resultado.IsSuccess);
         }
     }
 }
