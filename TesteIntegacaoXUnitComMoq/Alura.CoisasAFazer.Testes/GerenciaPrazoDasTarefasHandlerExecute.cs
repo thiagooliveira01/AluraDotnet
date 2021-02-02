2df5f530a2bdc99ee56fec74a303739a81de971a
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-02-02 20:51:52 -0300

    Capitulo4

--- TesteIntegacaoXUnitComMoq/Alura.CoisasAFazer.Testes/GerenciaPrazoDasTarefasHandlerExecute.cs
+++ TesteIntegacaoXUnitComMoq/Alura.CoisasAFazer.Testes/GerenciaPrazoDasTarefasHandlerExecute.cs
@@ -3,6 +3,7 @@ using Alura.CoisasAFazer.Core.Models;
 using Alura.CoisasAFazer.Infrastructure;
 using Alura.CoisasAFazer.Services.Handlers;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,6 +57,33 @@ namespace Alura.CoisasAFazer.Testes
             //assert
             var tarefasEmAtraso = repo.ObtemTarefas(t => t.Status == StatusTarefa.EmAtraso);
             Assert.Equal(5, tarefasEmAtraso.Count());
+        }
+
+        [Fact]
+        public void QuandoInvocadoDeveChamarAtualizarTarefasQtdeVezesDoTotalDeTarefasAtrasadas()
+        {
+            //arrange
+            var categ = new Categoria("Dummy");
+            var tarefas = new List<Tarefa>
+            {
+                //atrasadas a partir de 1/1/2019
+                new Tarefa(100, "Tirar lixo", categ, new DateTime(2018,12,31), null, StatusTarefa.Criada),
+                new Tarefa(104, "Fazer o almoço", categ, new DateTime(2017,12,1), null, StatusTarefa.Criada),
+                new Tarefa(109, "Ir à academia", categ, new DateTime(2018,12,31), null, StatusTarefa.Criada)
+            };
+
+            var mock = new Mock<IRepositorioTarefas>();
+            mock.Setup(r => r.ObtemTarefas(It.IsAny<Func<Tarefa, bool>>())).Returns(tarefas);
+            var repo = mock.Object;
+
+            var comando = new GerenciaPrazoDasTarefas(new DateTime(2019, 1, 1));
+            var handle = new GerenciaPrazoDasTarefasHandler(repo);
+
+            //act
+            handle.Execute(comando);
+
+            //assert
+            mock.Verify(r => r.AtualizarTarefas(It.IsAny<Tarefa[]>()), Times.Once());
         }
     }
 }
---
+++ TesteIntegacaoXUnitComMoq/Alura.CoisasAFazer.Testes/ObtemCategoriaPorIdExecute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Services.Handlers;
+using Alura.CoisasAFazer.Infrastructure;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class ObtemCategoriaPorIdExecute
+    {
+        [Fact]
+        public void QuandoIdForExistenteDeveChamarObtemCategoriaPorIdUmaUnicaVez()
+        {
+            //arrange
+            var idCategoria = 20;
+            var comando = new ObtemCategoriaPorId(idCategoria);
+            var mock = new Mock<IRepositorioTarefas>();
+            var repo = mock.Object;
+            var handler = new ObtemCategoriaPorIdHandler(repo);
+
+            //act
+            handler.Execute(comando);
+
+            //assert
+            mock.Verify(r => r.ObtemCategoriaPorId(idCategoria), Times.Once);
+        }
+    }
+}
