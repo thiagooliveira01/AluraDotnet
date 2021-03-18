2ae577a05bcf8d8cc77db6dcaaa3c9f667b2c4eb
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-03-18 18:04:24 -0300

    charppoo-curso5

---
+++ Fundamentos/csharppoo-curso5/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2018, 6, 20);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(40); //dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+        }
+    }
+}
