4ac28c354d62283ce7d54158caf070ee472b4c26
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-03-23 22:35:02 -0300

    Curso 8 Capitulo 1

---
+++ Fundamentos/csharppoo-curso8/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> idades = new List<int>();
+                    
+            idades.AdicionarVarios(1, 5, 14, 25, 38, 61);
+
+            for (int i = 0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
