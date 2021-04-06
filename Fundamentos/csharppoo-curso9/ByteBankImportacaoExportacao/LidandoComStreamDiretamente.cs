7f17ff10bf7d1759bef5bfe4245c099eab992a83
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-04-05 22:45:06 -0300

    Curso 9 aula 2

---
+++ Fundamentos/csharppoo-curso9/ByteBankImportacaoExportacao/LidandoComStreamDiretamente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void LidandoComFileStreamDiretamente()
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024]; // 1kb
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+            }
+        }
+
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = Encoding.UTF8;
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+
+            Console.Write(texto);
+
+        }
+    }
+}
---
+++ Fundamentos/csharppoo-curso9/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,32 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    partial class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+                using (var leitor = new StreamReader(fluxoDoArquivo))
+                {
+                    while (!leitor.EndOfStream)
+                    {
+                        var linha = leitor.ReadLine();
+                        Console.WriteLine(linha);
+                    }
+                }
+
+
+            Console.ReadLine();
+        }
+    }
+} 
+ 
